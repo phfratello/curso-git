9ea5a8502061c40eec4ab63792b59f682d0944d5
Paulo Henrique <pauloh.2903@gmail.com>
2021-01-22 14:21:01 -0300

    Acrescentada uma mensagem de bom dia

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
