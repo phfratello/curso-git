dca47d818e6c07126687d13cbd5be1a221019553
Paulo Henrique <pauloh.2903@gmail.com>
2021-01-22 14:17:58 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
